231ad6bb58a0e647800571e5221322585522b7ee
Joe Plevin <joe.plevin@hotmail.co.uk>
2017-11-22 21:49:33 +0000

    section 3 completed

--- w1d3_loopsArrays.rb
+++ w1d3_loopsArrays.rb
@@ -89,7 +89,6 @@ users["Erik"][:pets].store("Fluffy",:dog)
     dog: "Oswald"
     }
 }
-
 # C. Given the following data structure:
 
 
@@ -111,6 +110,19 @@ united_kingdom = [
 
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+united_kingdom[1][:capital] = "Cardiff"
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+united_kingdom << {
+  name: "Northern Ireland",
+  population: 1811000,
+  capital: "Belfast"
+}
 # 3. Use a loop to print the names of all the countries in the UK.
+for country in united_kingdom
+  p country[:name]
+end
 # 4. Use a loop to find the total population of the UK.
+population = 0
+for country in united_kingdom
+  population += country[:population]
+end
